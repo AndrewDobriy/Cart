b8d365985e4d6e3a496a2bb71e925248c38f8b5d
AndrewDobriy <an.dobriy@mail.ru>
2022-10-28 21:49:51 +0300

    first commit

---
+++ goods.js
@@ -0,0 +1,16 @@
+const GOODS = [{
+	id: 1,
+	name: 'Уроки по HTML',
+	price: 300,
+	imgSrc:'html.jpg',
+}, {
+	id: 2,
+	name: 'Уроки по CSS',
+	price: 150,
+	imgSrc:'css.jpg',
+}, {
+	id: 3,
+	name: 'Уроки по JS',
+	price: 260,
+	imgSrc:'js.png',
+}]
